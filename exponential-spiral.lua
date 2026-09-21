8ba1d483e74cf593496f5003991946ee6ce0b855
David Wang <124479657+fdformula@users.noreply.github.com>
2026-09-21 12:28:02 -0400

    Add files via upload

---
+++ exponential-spiral.lua
@@ -0,0 +1,17 @@
+-- by David Wang, dwang@liberty.edu, on 09/21/2026
+opts = {
+  t = {0, 2 * pi, 0.01},
+  k = {1, 20, default = 4},
+  m = {-4, -0.1, 0.1, default = -0.4},
+  xrange = {-1.2, 1.2}, yrange = {-1.2, 1.2},
+  resolution = 3000,
+  layout = {
+    width = 600, height = 600, square = true,
+    xaxis = { visible = false }, yaxis = { visible = false },
+    title = "<font size=4>Exponential spiral: <em>x</em>(<em>t</em>) = sin(<em>kt</em>) <em>e<sup>mt</sup></em>, <em>y</em>(<em>t</em>) = cos(<em>kt</em>) <em>e<sup>mt</sup></em></font>"
+  }
+}
+manipulate({
+    '@(t) sin(k*t)*exp(m*t)',
+    '@(t) cos(k*t)*exp(m*t)'
+  }, opts)
