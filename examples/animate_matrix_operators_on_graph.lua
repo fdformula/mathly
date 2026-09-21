318215cd24465cdcedee8ea63510a596d269b9c5
David Wang <124479657+fdformula@users.noreply.github.com>
2026-09-21 10:00:06 -0400

    Add files via upload

--- examples/animate_matrix_operators_on_graph.lua
+++ examples/animate_matrix_operators_on_graph.lua
@@ -59,6 +59,6 @@ opts = {
   r = {0, 1, 1, default = 0, label = 'Reflect about y = x'},
   xrange = {-6, 6}, yrange = {-6, 6}, controls = 'abqcdmnr',
   javascript = jcode,
-  layout = { width = 640, height = 640, square = true, title = '<h3>Matrix operators on a graph</h3>' }
+  layout = { width = 640, height = 640, square = true, title = '<font size=4>Matrix operators on a graph: <em>y</em> = <em>x - x </em>sin <em>x</em> + 2</font>' }
 }
 manipulate(fstr, opts)
