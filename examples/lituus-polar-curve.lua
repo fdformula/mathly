69ed499349a67ec47a3fe9b536cd757fcba9e4d0
David Wang <124479657+fdformula@users.noreply.github.com>
2026-09-23 21:22:48 -0400

    Add files via upload

--- examples/lituus-polar-curve.lua
+++ examples/lituus-polar-curve.lua
@@ -11,12 +11,12 @@ fstr1 = {'@(t) (' .. r .. ') * cos(t)', '@(t) (' .. r .. ') * sin(t)'}
 fstr2 = {'@(t) (-' .. r .. ') * cos(t)', '@(t) (-' .. r .. ') * sin(t)'}
 opts = {
   t = {0, 10 * pi, 0.01}, xrange = {-7, 7}, color = 'blue',
-  a = {1, 12, default = 4},
+  a = {1, 12, default = 9},
   javascript = jscode,
   layout = {
     width = 500, height = 500, square = true,
     xaxis = { visible = false }, yaxis = { visible = false },
-    title = '<font size=4>Lituus spiral: polar equation <em>r</em><sup>2</sup>(<em>&theta;</em>) = <em>a</em><sup>2</sup> / <em>&theta;</em></font>'
+    title = '<font size=4>Lituus spiral: polar equation <em>r</em><sup>2</sup> = <em>a</em><sup>2</sup> / <em>&theta;</em></font>'
   },
   enhancements={{x = fstr2[1], y = fstr2[2], color = 'red', width = 2}}
 }
